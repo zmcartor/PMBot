c13c9d0a92da0924eb0caa75abf329c546b0a98f
Zach <zmcartor@gmail.com>
2013-10-03 19:59:28 -0400

    BotRegex class holds message regex filters

--- lib/ircbot.rb
+++ lib/ircbot.rb
@@ -2,7 +2,7 @@ module PMBot
   class Botcore
     require 'cinch'
 
-    def initialize(config = {})
+    def initialize(config = OpenStruct.new)
       @bot = Cinch::Bot.new do
 
       configure do |c|
@@ -11,12 +11,22 @@ module PMBot
         c.nick = config.nick || "PMBOT"
       end
 
-      #various actions dispatched to the bot from IRC
-      on :message, "hello" do |m|
-        m.reply "How about a touchbase for Milestone #{rand(1..34)} ?"
+      # todo, factor out the responses from BotCore
+      # BotCore should only need to know howto communicate with IRC
+      on :message, Botregex.greetings do |m|
+        m.reply "Hello this is #{bot.nick}, how about a quick Milestone #{rand(1..34)} touchbase?"
       end
 
-      # todo, more messages ...
+      on :message, Botregex.goodbyes do |m|
+        m.reply "Hey thanks for joining us today, looking forward to bringing this one over the finish line"
+      end
+
+      #catch all has 1 in 10 chance of randomly saying stuff about your last message
+      chances = (1..10).to_a
+      on :message do |m|
+        m.reply "... .... ok ..." if chances.sample == 1
+      end
+
 
     end #end bot configuration
     end #init
@@ -25,4 +35,25 @@ module PMBot
       @bot.start
     end
   end #class
-end
+
+  # serve as interface for regexes when matching against messages
+  # maybe todo - bind together match and response in one object
+  class Botregex
+
+    class << self
+      def greetings
+        /hello|hi|yoyos|morning|good morning/
+      end
+
+
+      def goodbyes
+        /later|laterz|see ya|bye|goodbye|have a good one/
+      end
+
+      def tech_words
+        /web|restful|rails|json|api|iOS/
+      end
+    end
+  end  #end class
+
+end #end PMBot Module
